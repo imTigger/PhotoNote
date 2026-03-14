8a30fa8c603ab5837e301c41feb4e051d0c91add
Tiger Fok <tiger@tiger-workshop.com>
2026-03-14 18:07:55 +0000

    Added share to add photo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'dart:async';
 import 'screens/home_screen.dart';
+import 'screens/add_photos_screen.dart';
 import 'services/database_service.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,12 +12,67 @@ void main() async {
   runApp(const PhotoNoteApp());
 }
 
-class PhotoNoteApp extends StatelessWidget {
+class PhotoNoteApp extends StatefulWidget {
   const PhotoNoteApp({super.key});
+
+  @override
+  State<PhotoNoteApp> createState() => _PhotoNoteAppState();
+}
+
+class _PhotoNoteAppState extends State<PhotoNoteApp> {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  StreamSubscription? _intentDataStreamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Handle shared images when app is already running
+    _intentDataStreamSubscription = ReceiveSharingIntent.instance.getMediaStream().listen((List<SharedMediaFile> value) {
+      if (value.isNotEmpty) {
+        _handleSharedImages(value);
+      }
+    }, onError: (err) {
+      debugPrint("Error receiving shared media: $err");
+    });
+
+    // Handle shared images when app is opened via share
+    ReceiveSharingIntent.instance.getInitialMedia().then((List<SharedMediaFile> value) {
+      if (value.isNotEmpty) {
+        _handleSharedImages(value);
+      }
+      // Clear the initial shared media to prevent it from being processed again
+      ReceiveSharingIntent.instance.reset();
+    });
+  }
+
+  void _handleSharedImages(List<SharedMediaFile> sharedFiles) {
+    // Convert SharedMediaFile to XFile
+    final images = sharedFiles
+        .where((file) => file.type == SharedMediaType.image)
+        .map((file) => XFile(file.path))
+        .toList();
+
+    if (images.isNotEmpty) {
+      // Navigate to AddPhotosScreen with pre-selected images
+      navigatorKey.currentState?.push(
+        MaterialPageRoute(
+          builder: (context) => AddPhotosScreen(initialImages: images),
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _intentDataStreamSubscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       title: 'Photo Note',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
